391536b489113d0fb53011e1797ffcb42ac5dc82
Beatriz Schwartz Sabbatini <43048941+beatrizsabbatini@users.noreply.github.com>
2020-11-21 12:48:08 -0300

    feat: added resolution to the challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(res => {
+      setRepositories(res.data);
+    })
+  }, [])
   async function handleAddRepository() {
-    // TODO
+    const newRepository = {
+      title: 'MEI Fácil',
+      url: 'https://github.com/beatrizsabbatini/mei-facil',
+      techs: ['React Native', 'JavaScript']
+    }
+    
+    const response = await api.post('repositories', newRepository)
+
+    setRepositories([...repositories, response.data ])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(item => item.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((item, index) => {
+          return (
+            <li key={index}>
+              <h1>{item.title}</h1>
+              <button onClick={() => handleRemoveRepository(item.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        })}
+       
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
